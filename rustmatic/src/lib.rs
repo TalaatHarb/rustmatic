0cd8f15033c13ba83fb8bb0f77dd4ad0f195501c
Mohamed Talaat Harb <eng.talat.harb@gmail.com>
2024-03-09 13:54:49 +0200

    #3 added a test that confirms ndarray works

--- rustmatic/src/lib.rs
+++ rustmatic/src/lib.rs
@@ -5,10 +5,21 @@ pub fn add(left: usize, right: usize) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ndarray::prelude::*;
 
     #[test]
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn ndarray_works(){
+        let a = arr2(&[[1,2],[3,4]]);
+        let b = arr2(&[[5,6],[7,8]]);
+
+        let c = a.dot(&b);
+
+        assert_eq!(array![[19,22],[43,50]], c);
     }
 }
